9fa1e4cea1c4f9e10558110b32528b607bf9cde3
Shira Salingré <salingre@mail.tau.ac.il>
2019-11-06 09:46:55 +0200

    updated

--- R/meta.R
+++ R/meta.R
@@ -5,20 +5,19 @@ library(sf)
 library(extrafont)
 
 crete_locations <- read_csv("~/Lab stuff/Crete/CreteDives.csv")
-colnames(crete_locations) <- c()
+colnames(crete_locations) <- c("lon", "lat", "site", "dive")
 crete_locations
 
 # Load shapefile
 theme_set(theme_bw())
 gr_shp <- st_read("~/Lab stuff/Crete/greece shp/Crete.shp")
 
-# png("crete2019/Crete_locations.png", height = 800, width = 1600, bg = 'transparent')
+# png("Crete_locations.png", height = 800, width = 1600, bg = 'transparent')
 ggplot(crete_locations) +
   geom_sf(data = gr_shp) + 
   geom_jitter(aes(x = lon, y = lat, fill = site), cex = 10, alpha = 0.5, shape = 24) + 
-  geom_text(aes(x = lon, y = lat, label = dive), inherit.aes = TRUE) +
-  labs(title = "Crete survey locations", x = "", y = "", fill = "Site") + 
-  scale_fill_brewer(palette = "YlOrRd") +
+    labs(title = "Crete survey locations", x = "", y = "", fill = "Site") + 
+  scale_fill_brewer(palette = "Dark2") +
   theme(text = element_text(size = 16,  family = "Segoe UI"))
 # dev.off()
 
